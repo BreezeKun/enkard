0c636c15ddbd0a832db0d3714209d0e6e6aba25f
BreezeKun <akshadsatpute1989@gmail.com>
2026-08-25 17:42:07 +0530

    clone

---
+++ enkard/enka/enums/enum.py
@@ -0,0 +1,19 @@
+import sys
+
+if sys.version_info >= (3, 11):
+    from enum import StrEnum
+else:
+    from enum import Enum
+
+    class StrEnum(str, Enum):
+        pass
+
+
+__all__ = ("Game",)
+
+
+class Game(StrEnum):
+    """Game."""
+
+    GI = "gi"
+
---
+++ enkard/enka/models/gi/__init__.py
@@ -0,0 +1,6 @@
+from .build import *
+from .character import *
+from .costume import *
+from .icon import *
+from .player import *
+from .response import *
