9edac64d2f8fee27353b42d51c1d402cff9280d4
BreezeKun <akshadsatpute1989@gmail.com>
2026-08-29 19:03:54 +0530

    done

---
+++ test.py
@@ -0,0 +1,25 @@
+import json
+import asyncio
+from enkard.enkacard.utils.create_banner_one import generationOne
+from enkard.enka import GenshinClient, gi
+from enkard.enkacard.encbanner import CreateBanner
+
+async def main() -> None:
+    async with GenshinClient(gi.Language.ENGLISH) as client:
+        response = await client.fetch_showcase(1817389136)
+        data = response.model_dump()
+        response = CreateBanner(data=data).generate()
+        with open("test_result.json", "w", encoding="utf-8") as f:
+                    json.dump(response, f, indent=4, ensure_ascii=False)
+                    
+        for name, dat in response.items():
+            banner = await generationOne(
+                dat,
+                adapt=False,
+                lvl="Level",
+                uid="1817389136",
+                hide_uid=False,
+            )
+            banner.save(f"{name}.png")
+
+asyncio.run(main())
